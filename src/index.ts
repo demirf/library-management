55567dbc5ff8a78352fd7e62b464376d0a590eae
Ferhat Demir <edemirferhat@gmail.com>
2024-07-27 00:33:51 +0300

    feat: set up project and define initial folder structure

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { AppDataSource } from './config';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(error => console.log(error));
